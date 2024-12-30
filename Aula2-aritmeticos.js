dadf3ccc7452c6cbc97945d168f1470ff6c02ce8
Alex Santos <alexsantos.alss.13@gmail.com>
2024-12-30 18:13:18 -0300

    Sobre aual 2

---
+++ Aula2-aritmeticos.js
@@ -0,0 +1,30 @@
+let num = 2;
+
+console.log('Operadores Aritiméticos');
+console.log(num + 2); //Soma
+console.log(num - 2); //Subtração
+console.log(num * 2); //Multiplicação
+console.log(num / 2); //Divisão
+console.log(num % 2); //Módulo
+
+console.log('Exponenciação');
+console.log(num ** 2);
+
+console.log('Aritiméticos com atribuição');
+console.log(num += 1);
+console.log(num -= 1);
+console.log(num *= 1);
+console.log(num /= 1);
+console.log(num %= 1);
+
+console.log('Incremento e Decremento');
+
+//Incrementa depois de executar
+num++;
+console.log(num);
+num--;
+console.log(num);
+
+//Incrementa antes 
+console.log(++num);
+console.log(--num);
